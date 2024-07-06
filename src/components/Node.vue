defa9f7ba2bb42a229973e4dc3457ce3da1f30c4
Yusuf Özdemir <yusuf@ozdemir.be>
2024-07-06 10:42:52 +0300

    Update Node.vue

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -1,18 +1,25 @@
 <template> 
-    <div class="space-2 justify-center transition-all duration-300 ">
-        <div class=" border rounded h-16 w-40 mx-auto bg-slate-50" ref="element">
-           <div>{{ item.id }} ({{ nodes.length }})</div>
-           <div>{{ item.name }} </div>
-            <div class="text-center">
-                <button class="border px-1 bg-white" v-if="nodes.length" @click="handleChildren">{{ show ? '-' : '+' }}</button>
-            </div>
+    <div class="space-2 justify-center transition-all duration-300 relative">
+        <div class="border rounded h-16 w-40 mx-auto bg-slate-50"
+             :class="{
+              'before:absolute before:border-l before:w-1 before:h-4 before:-mt-4': item.parentId,
+              // 'after:absolute after:border-l after:w-1 after:h-4 after:-mb-4': nodes.length && show
+        }"
+              ref="element">
+
+           <div>
+             <div>{{ item.id }} ({{ nodes.length }})</div>
+             <div>{{ item.name }} </div>
+              <div class="text-center">
+                  <button class="border w-4 h-4 bg-white text-xs" v-if="nodes.length" @click="handleChildren">{{ show ? '-' : '+' }}</button>
+              </div>
+           </div>
         </div>
+        <div v-if="nodes.length && show" class="absolute border-l left-1/2 w-1 h-2"></div>
 
         <Transition name="list">
-            <NodeContainer v-if="nodes.length && show" class=" mt-5 space-x-5">
-
+            <NodeContainer v-if="nodes.length && show" class="mt-4 space-x-5">
                 <Node v-for="(node,index) in nodes" :key="node.id" :id="node.id" />
-  
             </NodeContainer>
         </Transition>
     </div>
